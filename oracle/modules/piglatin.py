be899e9e626be2e4f07e512e934edc260ff9a230
Alex Matheson <amatho250@gmail.com>
2014-04-29 12:34:12 +1000

    Update piglatin.py

--- oracle/modules/piglatin.py
+++ oracle/modules/piglatin.py
@@ -14,16 +14,31 @@ def piglatin(l, b, i):
     else:
         out = ''
         vowel_pattern = re.compile(r'^[aeiouAEIOU]')    
-        consonant_pattern = re.compile(r'^([^aeiouAEIOU]*)(.*)')    
+        consonant_pattern = re.compile(r'^([^aeiouAEIOU]+)(.*)')    
+        punc_s_pattern = re.compile(r'^(\W+)')
+        punc_e_pattern = re.compile(r'(\W+)$')
         for word in i.args:
-            print word
+            punc_s = ''
+            punc_e = ''
+            
+            psp_match = punc_s_pattern.match(word)
+            if psp_match is not None:
+                punc_s = psp_match.group(1)
+                word = word.replace(punc_s, '')
+            
+            pep_match = punc_e_pattern.match(word)
+            if pep_match is not None:
+                punc_e = pep_match.group(1)
+                print punc_e
+                word = word.replace(punc_e, '')
+            
             c_v_match = vowel_pattern.match(word)
             if c_v_match is not None:
-                out += word + 'ay '
+                out += punc_s + word + 'ay' + punc_e + ' '
                 continue
             c_p_match = consonant_pattern.match(word)
             if c_p_match is not None:
-                out += c_p_match.group(2) + c_p_match.group(1) + 'ay '
+                out += punc_s + c_p_match.group(2) + c_p_match.group(1) + 'ay' + punc_e + ' '
         b.l_say('%s: %s' % (i.nick, out), i, 1)
         return True
             
@@ -35,11 +50,30 @@ def opish(l, b, i):
     !r user
     """
     if i.args[0] == '-':
-        pass
-    else:
-        #Adds -op after every consonant
         out = ''
-        #consonant_pattern = re.compile(r'
+        for word in i.args[1:]:
+            out += word.replace('op',''),
+        b.l_say('%s: %s' % (i.nick, out), i, 1)
+        return True
+    else:
+        #Adds -op after every consonant group
+        out = ''
+        vowels = re.compile(r'[aeiouAEIOU]')
+        last_consonant = False
+        for word in i.args:
+            for letter in word:
+                vowel_match = vowels.match(letter)
+                if vowel_match is not None:
+                    if last_consonant == True:
+                        out += 'op'
+                    last_consonant == False
+                    out += letter
+                    continue
+                else:
+                    last_consonant = True
+        out += letter
+        b.l_say('%s: %s' % (i.nick, out), i, 1)
+        return True
     b.l_say('Sorry! This has not been implemented yet.', i, 0)
 
 def turkeyirish(l, b, i):
@@ -48,6 +82,7 @@ def turkeyirish(l, b, i):
     !a <-> [message...]
     !r user
     """
+    #Add -ab after every consonant group
     b.l_say('Sorry! This has not been implemented yet.', i, 0)
 
 def doubledutch(l, b, i):
