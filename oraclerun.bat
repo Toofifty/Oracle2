c3aff60fc8f295fef299e319f8054280183e691e
Toofifty <amatho250@gmail.com>
2014-04-29 22:48:20 +1000

    Removed .bats

--- oraclerun.bat
+++
@@ -1,6 +0,0 @@
-@echo OFF
-cd oracle
-:LOOP
-python bot.py --pass wombat1 --channel #rapid
-pause>nul
-goto LOOP
--- quickrun.bat
+++
@@ -1,6 +0,0 @@
-@echo OFF
-cd oracle
-:LOOP
-python bot.py --nick Oracle_ --verbose --channel #rapid
-pause>nul
-goto LOOP
